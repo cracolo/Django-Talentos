2621918eb3c28d757e1614d12177b89378f775a9
cracolo <cracolo@gmail.com>
2026-06-14 19:44:55 -0400

    modificando urls

--- alumnos/urls.py
+++ alumnos/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path    
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path('', views.index, name='index'),
+ path('nosotros/', views.nosotros, name='nosotros'),
+ path('contacto/', views.contacto, name='contacto'),
 ]   
--- alumnos/views.py
+++ alumnos/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, 'alumnos/index.html', context)   
+def nosotros(request):
+    return render(request, 'alumnos/html/nosotros.html')
+def contacto(request):
+    return render(request, 'alumnos/html/contacto.html')
